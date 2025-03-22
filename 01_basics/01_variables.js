253d54432b1ca0d50dc010dd1137e9dad7ea6346
rehmanjatoirj <k247813@nu.edu.pk>
2025-03-22 19:00:27 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountID ="2542";
+let accountEmail = "hc@hitesh.com";
+var accountPass = "2323232";
+accountCity = "Jaipur";
+console.table([accountEmail,accountID,accountPass,accountCity]);
