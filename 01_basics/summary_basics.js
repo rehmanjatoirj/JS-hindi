c7b283e0340fcb48a0a141c61dec9369e2713e49
Rehman Ali <k247813@nu.edu.pk>
2025-03-27 13:39:17 +0000

    jatoi

---
+++ 01_basics/summary_basics.js
@@ -0,0 +1,9 @@
+/*here is total summary basic of java script such as variables ,strings , num , boolean , int  */
+
+let x = 43;
+y = 23;
+log.console(x+y);
+
+// git add .
+// git commit -m "fices"
+// git push
